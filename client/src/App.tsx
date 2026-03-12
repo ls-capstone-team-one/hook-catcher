c4ecd60100ad61bec7bfe477ee05849ebeed326f
Jonathan <33232878+unlikelykoala@users.noreply.github.com>
2026-03-12 16:38:10 -0400

    Fix: remove navbar from App.tsx

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -5,12 +5,8 @@ import BinView from "@/features/bin-view/BinView"
 function App() {
   return (
     <Router>
-      {/* put navbar here bc all routes will have navbar
-          will need conditional rendering for buttons 
-      */}
-      {/* <NavBar /> */}
       <Routes>
-        {/* Home page: will include NewBinCreator, BinList, SuccessModal, FailureModal */}
+        {/* Home page: will include NewBinCreator, BinList, CreateResultModal */}
         <Route path="/" element={<Home />} />
         {/* Bin page: will include bin info and request list */}
         <Route path="/bins/:id" element={<BinView />} />
