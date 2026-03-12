1162e62acf9b8878abd06cff93e624feca2f31e7
Tiia Rikama <95940578+tiiarikama@users.noreply.github.com>
2026-03-11 18:23:34 -0700

    feat: backend websocket integration (#13)
    * feat: backend websocket integration + new type for broadcasted request
    
    * chore: removed old todo comment

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import app from "./app";
+import wsManager from "./websockets/connectionManager";
+import http from "http";
 
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => {
+const server = http.createServer(app);
+
+wsManager.init(server);
+
+server.listen(PORT, () => {
   console.log(`HookCatcher server listening on port ${PORT}`);
 });
